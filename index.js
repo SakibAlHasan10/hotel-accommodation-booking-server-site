1b9cc5d5d26a66f1fcb59f14e80b6e0007d9b9bc
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-06 14:06:05 +0600

    added token and set cookie

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 const uri = process.env.SECRET_URI;
 
@@ -37,7 +43,14 @@ async function run() {
   }
   //   create user token 
   app.post('/jwt', async(req, res)=>{
-
+    const user = req.body
+    const token = jwt.sign(user, process.env.SECRET_TK, {expiresIn: '1h'})
+    res
+    .cookie("token", token, {
+        httpOnly:true,
+        secure:false
+    })
+    .send(token)
   })
 //   post user
 app.post("/users", async(req, res)=>{
