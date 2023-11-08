ce983fdbb355cd873a592b5d44116b481945e375
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-11-08 23:25:45 +0600

    added vercel

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://booking-aeff8.web.app"],
     credentials: true,
   })
 );
